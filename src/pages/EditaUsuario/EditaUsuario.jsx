47cf2593cf9bf29d7eb3bbbfad85da8ca8f52abe
Fernanda-Franco <fernandaf.vet@gmail.com>
2023-03-22 17:22:08 -0300

    useNavigate

--- src/pages/EditaUsuario/EditaUsuario.jsx
+++ src/pages/EditaUsuario/EditaUsuario.jsx
@@ -1,18 +1,26 @@
 import { Button } from "react-bootstrap";
 import { usuarios } from "../../data/usuarios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export function EditaUsuario() {
 
     // useParams é uma função que retorna um objeto
     //Este objeto contem os parametros passados pela rota
-    
-    let params = useParams();
+        let params = useParams();
     let id = params.id;
     
     const usuario = usuarios.find(usuario => {
         return usuario.id === parseInt(id); // retorna valor booleano que satisfaza a pesquisa
     });
+
+let navigate = useNavigate();
+
+    function editar() {
+        //Capturar as informações do formulário
+        //Validar os dados de entrada
+        //Modificar as informações no banco de dados
+        navigate("/usuarios")
+    }
 
     return (
         <div className="edita-usuario">
